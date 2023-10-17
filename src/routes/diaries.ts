5d9c235d5e01e51dcdae30cb96b8094bf01ec632
Jandueza <jonathanandueza1201@gmail.com>
2023-10-17 11:16:56 -0300

    first commit

---
+++ src/routes/diaries.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    res.send('TODO');
+});
+
+router.post('/', (_req, res) => {
+    res.send('TODO');
+});
+
+export default router;
